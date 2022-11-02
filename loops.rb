a1c51afbf616e93cadaadf18aff04a44377e4a97
haneenIzzaldeen <hjaradeh@everconnect.com>
2022-11-02 16:59:25 +0300

    loops

---
+++ loops.rb
@@ -0,0 +1,48 @@
+
+# number = 95
+# guess = ""
+
+# while(number != guess.to_i and number > 1 and number < 100)
+#   puts "enter the number"
+#    guess = gets.to_i
+
+# unless(guess == number)
+ 
+
+# else 
+# 	puts "yes it's correct"
+
+#  end
+# end
+
+
+# number = 95
+# guess = ""
+# loop do 
+#   puts "enter the number"
+#   guess = gets.to_i
+#   if (number == guess)
+# 	   puts "yes it's correct"
+#   break
+
+#   elsif (guess > number )
+#        puts "too high"
+
+#   elsif (guess < number )
+#        puts "too low"
+
+#  end
+# end
+
+
+# number = 95
+# guess =""
+# until number == guess 
+# puts "guess the number"
+# guess = gets.to_i 
+# end
+# puts "you guessed the number"
+
+
+
+
