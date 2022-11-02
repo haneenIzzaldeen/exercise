db4914ff768a8f4edaf825aa8be195d770972f1a
haneenIzzaldeen <hjaradeh@everconnect.com>
2022-11-02 08:25:22 +0300

    made some changes

---
+++ test.rb
@@ -0,0 +1,81 @@
+#  x=5
+#  puts x
+#  print  x
+#  print x
+
+#  x = 5
+
+#  while x <= 10 
+# puts "*"
+# x = x+1
+# end 
+
+# num = 20
+# puts ("hello " + num.to_s)
+
+# num = 20 
+# num2 = 30
+# puts (num.to_s + num2.to_s)
+
+# secret_word = "hello"
+# guess = ""
+
+# while guess != secret_word
+# puts "enter guess"
+# guess = gets.chomp()
+# end 
+# puts "you won"
+
+
+# loop do 
+# puts "hello"
+
+# a= 9
+# if a != 10 
+#     break
+# end
+# end 
+
+
+# ismale = true 
+# istall = true
+# if !ismale or !istall
+#     puts "yes"
+# else 
+# puts "no"
+# end
+
+
+
+# a = 40 
+# if a >= 50 and a <= 60
+#     puts "D"
+# elsif a >= 70 and a <=80
+#     puts "B"
+# else 
+#     puts "c"
+# end 
+
+
+# # var = 5 
+# # a = (var>2) ? "yes" : "no" ;
+# # puts a
+
+
+# def hello 
+
+# puts "hello world"
+
+# end 
+
+# hello
+
+
+# def hello(var = "hello" , var1 ="hello1")
+
+# puts ("hello world" + var + var1)
+
+# end
+# hello "haneen" , "haneen1"
+
+puts "hello world"
